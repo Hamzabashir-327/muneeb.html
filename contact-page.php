4c679cb09e150fd99beda84aa50a04a7f7cc2f76
Hamzabashir-327 <hamza327.hb@gmail.com>
2024-06-04 09:05:32 +0500

    Add files via upload

---
+++ contact-page.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>CONTACT</title>
+	<link rel="stylesheet" href="css/style.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <!--font icon--->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" integrity="sha512-BnbUDfEUfV0Slx6TunuB042k9tuKe3xrD6q4mg5Ed72LTgzDIcLPxg6yI2gcMFRyomt+yJJxE+zJwNmxki6/RA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+	 <script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
+  const revealElements = document.querySelectorAll('.reveal-element');
+
+  function revealOnScroll() {
+    revealElements.forEach(function(element) {
+      const elementTop = element.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      // Check if the element is in the viewport
+      if (elementTop < windowHeight * 0.75) {
+        element.classList.add('reveal-visible');
+      } else {
+        element.classList.remove('reveal-visible');
+      }
+    });
+  }
+
+  // Call revealOnScroll on page load and scroll
+  revealOnScroll();
+  window.addEventListener('scroll', revealOnScroll);
+});
+  </script>
+	<div class="MyDiv">
+			<section class="contact-sec-1">
+				<div class="container">
+				<div class="row reveal-element">
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+						<div class="main-div-1">
+							<div class="logo">
+								<div class="logo-img">
+								<img src="Images/Vector-Smart-Object.png" alt="">
+								</div>
+									<div class="AboutUS-RightText icon-box">
+						          <i class="call"><img src="Images/top-call.png"></i>
+						          <div class="IconText">
+						            <h5>Puhelinnumero</h5>
+						            <p><a href="tel:+47 333 78 901">+47 333 78 901</a></p>
+						          </div>
+						        </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div class="main-nav">
+                      	<ul>
+                      		<li><a href="autohoulto.php">Etusivu</a></li>
+                      		<li><a href="contact-page.php">Asiakaspalvelu</a></li>
+                      		<li><a href="#">Yritys</a></li>
+                      		<li><a href="#">Huoltotarjoukset</a></li>
+                      		<li><a href="#">Merkkikohtaiset</a></li>
+                      		<li><a href="#">huollot</a></li>
+                      		<li><a href="#">Huoltopalvelut</a></li>
+                      		<li><a href="#">Korjaamopalvelut</a></li>
+                      		<li><a href="#">Palvelut</a></li>
+                      		<li><a href="#">Kysy Whatsapilla</a></li>
+                      		<li><a href="#">Varaa huolto</a></li>           
+                      	</ul>
+                      </div>
+				</div>
+				<h1 class="Asiskaspalvelu">Asiskaspalvelu</h1>
+				</div>
+			</div>
+			</section>
+			<section class="contact-last-sec">
+				<div class="container">
+					<div class="row reveal-element">
+						<div class="col-lg-12 col-md-12 col-sm-12">
+							<div class="img-benner">
+					            <img src="Images/Color-Fill-1-copy-2.png">
+				           </div>
+				           <div class="detail">
+				           	<h1>Autohuolto Konala Asiakaspalvelu</h1>
+				           	<hr>
+				           	<p>Konalan Autohuolto Oy<br>
+Ristipellontie 17 00390 HELSINKI<br>
+010 3152 840 (8,35 snt/puhelu + 16,69 snt/min)<br><br>
+
+asiakaspalvelu@konalanautohuolto.fi<br>
+Avoinna: ma – pe 8:00 – 16:00<br><br>
+
+Konalan Autohuollon puhelinpalvelu palvelee vuoden jokaisena päivänä 07:00 – 20:00<br>
+<span class="spn-red-contact">010 3152 840</span><br><br>
+
+Palvelemme asiakkaitamme<br><br>
+
+– puhelimiella<br>
+– sähköpostilla<br>
+– yhteydenottolomakkeella<br>
+– WhatsAppilla</p>
+				           </div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="address">
+        	<div class="container">
+        		<div class="row reveal-element">
+        			<div class="col-lg-12">
+        				<div class="address-main">
+        					<h1>EMTE OY AVOINNA</h1>
+        					<hr>
+        					<div class="AboutUS-RightText icon-box1">
+						          <i class="time"><img src="Images/time.png"></i>
+						          <div class="IconText1">
+						            <h5>ma-pe 07:30-17:00</h5>
+						          </div>
+						    </div>
+						    <div class="AboutUS-RightText icon-box1">
+						          <i class="time"><img src="Images/loc.png"></i>
+						          <div class="IconText1">
+						            <h5>Ristipellontie 17 HELSINKI </h5>
+						          </div>
+						    </div>
+						    <p>Emte Oy puhelinpalvelu palvelee vuoden jokaisena päivänä 07:00 - 20:00.</p>
+						    <div class="AboutUS-RightText icon-box2">
+						          <i class="cont"><img src="Images/1.png"></i>
+						          <div class="IconText2">
+						            <h5>010 31 52 840</h5>
+						          </div>
+						    </div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+             <section class="footer">
+       	<div class="container">
+       		<div class="row reveal-element">
+       			<div class="logo-footer">
+						    	<img src="Images/Vector-Smart-Object10.png">
+						    </div>
+       			<div class="col-lg-4 col-md-4 col-sm-12">
+       				<div class="AboutUS-RightText icon-box3">
+						          <i class="foot"><img src="Images/-.png"></i>
+						          <div class="IconText3">
+						            <h5>010 3152 840 (8,35 snt/puhelu + 16,69 snt/min)</h5>
+						          </div>
+						    </div>
+						    <div class="AboutUS-RightText icon-box3">
+						          <i class="foot"><img src="Images/msg-.png"></i>
+						          <div class="IconText3">
+						            <h5>asiakaspalvelu@autohuoltokonala.fi</h5>
+						          </div>
+						    </div>
+       			</div>
+       			<div class="col-lg-4 col-md-4 col-sm-12">
+       				<div class="AboutUS-RightText icon-box3">
+						          <i class="foot"><img src="Images/loc-red-.png"></i>
+						          <div class="IconText3">
+						            <h5>Ristipellontie 17 00390 HELSINKI</h5>
+						          </div>
+						    </div>
+						    <div class="AboutUS-RightText icon-box3">
+						          <i class="foot"><img src="Images/time-red-.png"></i>
+						          <div class="IconText3">
+						            <h5>Avoinna: ma - pe 7:30 - 17:00</h5>
+						          </div>
+						    </div>
+       			</div>
+       			<div class="col-lg-4 col-md-4 col-sm-12">
+						    <div class="AboutUS-RightText icon-box3">
+						          <i class="foot"><img src="Images/gg.png"></i>
+						          <div class="IconText3">
+						            <h5>Laskutusohjeet </h5>
+						          </div>
+						    </div>
+       			</div>
+       			<div class="col-lg-12 col-md-12 col-sm-12">
+						    <div class="whatsapp-link">
+						    	<a aria-label="Chat on WhatsApp" href="https://wa.me/045 1860 497"> <img class="wa-img" alt="Chat on WhatsApp" src="Images/whatsapp.png" />Kysy tarjous WhatsApilla 045 1860 497</a>
+						    </div>
+       			</div>
+       			<div class="col-lg-12 col-md-12 col-sm-12">
+						    <div class="copy-right">
+						    	<h4>© 2021 Emte Oy. Privacy Policy.</h4>
+						    </div>
+       			</div>
+       		</div>
+       	</div>
+       </section>
+
+
+
+
+
+
+
+
+
+
+
+	</div>
+</body>
+</html>
